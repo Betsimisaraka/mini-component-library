83ced92d006ec4229e734f7b4e19066ba96dcf75
Betsimisaraka <anita.ras@onja.org>
2022-04-07 10:17:55 +0300

    Started the input component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,25 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Styles = {
+  small: {
+    border: 1,
+    iconSize: 16,
+    fontSize: 14,
+    lineHeight: 16,
+    paddingV: 4,
+    top: 4
+  },
+  large: {
+    border: 2,
+    iconSize: 24,
+    fontSize: 18,
+    lineHeight: 21,
+    paddingV: 8,
+    top: 8
+  }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +32,65 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+
+  const styles = Styles[size];
+
+  return (
+  <Wrapper style={{'--border': styles.border + "px", '--width': width + "px"}}>
+
+    <IconWrapper style={{'--size': styles.iconSize + "px", '--top': styles.top + "px"}}>
+      <Icon id={icon} strokeWidth={1} size={styles.iconSize} />
+    </IconWrapper>
+    <VisuallyHidden>{label}</VisuallyHidden>
+    <InputElement 
+      style={{'--width': width + "px", '--font': styles.fontSize + "px", '--line': styles.lineHeight + "px", '--padding-v': styles.paddingV + "px"}}
+      type="text"
+      placeholder={placeholder}
+    />
+  </Wrapper>
+  )
 };
+
+const Wrapper = styled.div`
+  border-bottom: var(--border) solid black;
+  position: relative;
+  width: max-content;
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: var(--top);
+  lef: 0;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+`
+
+const InputElement = styled.input`
+  outline: none;
+  border: none;
+  padding: var(--padding-v) 0rem var(--padding-v) 2rem; 
+  font-weight: 700;
+  font-size: var(--font);
+  line-height: var(--line);
+  color: ${COLORS.gray700};
+  width: var(--width);
+
+  &::placeholder {
+    font-weight: 400;
+    font-size: var(--font);
+    line-height: var(--line);
+    color: ${COLORS.gray500}; 
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+  &:focus {
+    border: 2px solid ${COLORS.black};
+  }
+
+`
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,26 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <SelectElement value={value} onChange={onChange}>
       {children}
-    </select>
+    </SelectElement>
   );
 };
+
+const SelectElement = styled.select`
+  background: rgba(128, 128, 128, 0.15);
+  border-radius: 8px;
+  color: #656565;
+  border: none;
+  padding: 12px 16px;
+
+  &::focus{
+    border: 1px solid blue;
+  }
+
+  &:hover{
+    color: #000000;
+  }
+`;
 
 export default Select;
